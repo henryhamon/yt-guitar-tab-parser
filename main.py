11f6b8ea5e3aa2f9964ffd93e21bb7a9f3ccac55
Henry Hamon <henryhamon@users.noreply.github.com>
2025-03-11 10:33:19 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from download_frames import download
 from extract_tab_pics import extract_tab
 from clean_tab import make_pdf
 
-main_directory = 'C:/Users/rohin/Desktop/projects/guitar_tab/frames'
+main_directory = '/content/'
 clear_all(main_directory)
 download(main_directory)
 extract_tab(main_directory)
